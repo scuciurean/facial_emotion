861e7466205d9d8624994963e53f4df5397fcbac
Sergiu <cuciurean.sergiu@yahoo.com>
2020-11-28 02:11:47 +0200

    Add a sobel filtering implementation

---
+++ imfilters.py
@@ -0,0 +1,33 @@
+from util import rgb2gray
+from scipy import signal
+import numpy as np
+import math as m
+
+"""
+    Sobel:
+        Gx = np.array([[-1,  0,  1], [-2,  0,  2], [-1,  0,  1]])
+        Gy = np.array([[-1, -2, -1], [ 0,  0,  0], [ 1,  2,  1]])
+
+    Sobel-Feldman:
+        Gx = np.array([[-3,  0,  3], [-10,  0, 10], [-3,  0,  3]])
+        Gy = np.array([[-3,-10, -3], [  0,  0,  0], [ 3, 10,  3]])
+
+    Scharr:
+        Gx = np.array([[-47,   0,  47], [-162,   0, 162], [-47,   0, 47]])
+        Gy = np.array([[-47,-162, -47], [   0,   0,   0], [ 47, 162, 47]])
+"""
+
+def sobel(img, thresh=70):
+    Gx = np.array([[-1,  0,  1], [-2,  0,  2], [-1,  0,  1]])
+    Gy = np.array([[-1, -2, -1], [ 0,  0,  0], [ 1,  2,  1]])
+    dx = signal.convolve2d(img, Gx, boundary='symm', mode='same')
+    dy = signal.convolve2d(img, Gy, boundary='symm', mode='same')
+    mag = np.zeros(dx.shape)
+    ang = np.zeros(dy.shape)
+    for x in range(0, mag.shape[0]):
+        for y in range(0, mag.shape[1]):
+            # TODO: Check if magnitude trhesholding is useful or not
+            mag[x, y] = m.sqrt(dx[x][y]**2 + dy[x][y]**2) * (mag[x, y] < thresh)
+            ang[x, y] = m.atan2(dy[x][y] , dx[x][y]) * 180 / m.pi * (dx[x][y] > 0)            
+
+    return mag, ang
---
+++ util.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def rgb2gray(rgb):
+	data = (0.2989 * rgb[:,:,0] + 0.5870 * rgb[:,:,1] + 0.1440 * rgb[:,:,2])
+	data = data / data.max() * 255 # Normalizes data in range 0 - 255
+	return data.astype(np.uint8)
